79669115b578b8697c7700bed25d2dc8e97d9506
takasimamasahiro <134473651+takasimamasahiro@users.noreply.github.com>
2025-05-25 19:11:20 +0900

    [uppdate]　志慶真に言われた非静的イベントにしてランキングを追加した。あとは保存したいときに読んで、取得するときに読んだらたぶん行ける

--- Assets/System/GameSystem.cs
+++ Assets/System/GameSystem.cs
@@ -8,6 +8,11 @@ public class GameSystem : MonoBehaviour
 {
     [SerializeField, Header("制限時間")] private float _timeLimit = 1.0f;
 
+
+    //勝手に書き変わらないようにする
+    public static GameSystem Instance { get; private set; }
+
+    //（　＾ω＾）・・・ココで使ってるやんevent…
     [SerializeField, Header("ゲームオーバーのイベント")]
     private UnityEvent _eventOnGO;
 
@@ -22,35 +27,93 @@ public class GameSystem : MonoBehaviour
     //ちゃっぴーに聞いたら多分こんな奴だと思う
 
     //スコア変更イベント定義（float = 新しいスコア）
-    public static event Action<float> OnScoreChanged;
+    event Action<float> OnScoreChanged;
     //打数変更イベント定義（int = 新しい打数）
-    public static event Action<int> OnDasuuChanged;
+    event Action<int> OnDasuuChanged;
 
     //このイベントしたなかったな勉強になた。
+    //プレイヤーが打数のやつ読んでくれるからこっちで制限をする
 
-    // Start is called before the first frame update
-    void Start()
+
+    //ランキングの実装
+    //上位ｘ名残す
+    const int MaxRankingCount = 5;
+
+    void Awake()
     {
+        Instance = this;
+    }
+
+    public void ResetScore()
+    {
+        _score = 0;
+        _dasuu = 0;
+        OnScoreChanged?.Invoke(_score);
+        OnDasuuChanged?.Invoke(_dasuu);
 
     }
-    void Update()
-    {
 
-    }
     public void AddScore(float amount)
     {
         _score += amount;
-
+        //スコアをマイナスするときマイナスにならないようにする
+        if (_score < 0)
+        {
+            _score = 0;
+        }
         //ここで発火スコア
         OnScoreChanged?.Invoke(_score);
     }
 
+    //public void SubtractScore(float amount)
+    //{
+    //    _score -= amount;
+    //    if (_score < 0) _score = 0;
+    //    OnScoreChanged?.Invoke(_score);
+    //}
     public void AddDasuu(int amount)
     {
         _dasuu += amount;
 
         //ここで発火打数
         OnDasuuChanged?.Invoke(_dasuu);
+
+    }
+
+
+    //ランキングの保存
+    public void SaveScoreToRanking()
+    {
+        List<float> ranking = LoadRanking();
+        ranking.Add(_score);
+        ranking.Sort((a, b) => b.CompareTo(a));
+        //上位のｘ件に
+        if (ranking.Count > MaxRankingCount)
+        {
+            ranking = ranking.GetRange(0, MaxRankingCount);
+        }
+
+        //保存
+        for (int i = 0; i < ranking.Count; i++)
+        {
+            PlayerPrefs.SetFloat($"Ranking_{i}", ranking[i]);
+        }
+        PlayerPrefs.Save();
+    }
+    //ランキングの取得
+    public List<float> LoadRanking()
+    {
+        List<float> ranging = new List<float>();
+
+        for (int i = 0; i < MaxRankingCount; i++)
+        {
+            if (PlayerPrefs.HasKey($"Ranking_{i}"))
+            {
+                float score = PlayerPrefs.GetFloat($"Ranking_{i}");
+                ranging.Add(score);
+            }
+        }
+        return ranging;
 
     }
 }
